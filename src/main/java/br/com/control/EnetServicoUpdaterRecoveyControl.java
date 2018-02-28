9bf11ffe7d41c485926bd2e527181a70a91bfa5b
jose.renato <renatoalvesbelem@gmail.com>
2018-02-28 12:55:22 -0300

    Corrigido update e insert

--- src/main/java/br/com/control/EnetServicoUpdaterRecoveyControl.java
+++ src/main/java/br/com/control/EnetServicoUpdaterRecoveyControl.java
@@ -12,15 +12,15 @@ import java.io.IOException;
 
 public class EnetServicoUpdaterRecoveyControl {
 
-    private static final String path = "C:\\Projetos\\WEB_PRODUTO\\fWEB\\Produto\\Documentos\\Roteiro de Teste\\Configuração ENETSERVICO\\SC\\Planilha\\TRE CORRIGIDO\\UPDATE ORIGINAL completo-TRE.txt";
+    private static final String path = "C:\\Projetos\\WEB_PRODUTO\\fWEB\\Produto\\Documentos\\Roteiro de Teste\\Configuração ENETSERVICO\\SC\\Planilha\\TRE CORRIGIDO\\INSERT completo-TRE- update.txt";
 
     public static void main(String[] args) throws IOException, BiffException {
-        String updateSQL = "update ENETSERVICO set ";
+        String updateSQL = "UPDATE ENETSERVICO SET ";
 
         WorkbookSettings ws = new WorkbookSettings();
         ws.setEncoding("Cp1252");
 
-        Workbook workbook = Workbook.getWorkbook(new File("C:\\Projetos\\WEB_PRODUTO\\fWEB\\Produto\\Documentos\\Roteiro de Teste\\Configuração ENETSERVICO\\SC\\Planilha\\TRE CORRIGIDO\\UPDATE ORIGINAL completo-TRE.xls"), ws);
+        Workbook workbook = Workbook.getWorkbook(new File("C:\\Projetos\\WEB_PRODUTO\\fWEB\\Produto\\Documentos\\Roteiro de Teste\\Configuração ENETSERVICO\\SC\\Planilha\\TRE CORRIGIDO\\INSERT completo-TRE.xls"), ws);
 
         BufferedWriter writer = new BufferedWriter(new FileWriter(new File(path)));
 
@@ -32,11 +32,28 @@ public class EnetServicoUpdaterRecoveyControl {
             String CDSERVICO = sheet.getCell(0, i).getContents();
             String FLFORAUSO = sheet.getCell(1, i).getContents();
             String CDSERVICOPAI = sheet.getCell(2, i).getContents();
-            String DEITEMMENU = sheet.getCell(3, i).getContents();
+            String DETITULO = sheet.getCell(3, i).getContents();
+            String FLNECESSITAPF = sheet.getCell(4, i).getContents();
+            String FLNECESSITAOAB = sheet.getCell(5, i).getContents();
+            String FLNECESSITACERT = sheet.getCell(6, i).getContents();
             String NUORDEM = sheet.getCell(7, i).getContents();
-            String FLVISIVELMENU = sheet.getCell(11, i).getContents();
+            String FLINTERMEDIARIO = sheet.getCell(8, i).getContents();
+            String FLINICIAL = sheet.getCell(9, i).getContents();
+            String FLVISIVELMENU = sheet.getCell(10, i).getContents();
+            String FLACESSORAPIDO = sheet.getCell(11, i).getContents();
 
-            String sql = (updateSQL + "FLFORAUSO ='" + FLFORAUSO + "',CDSERVICOPAI =" + CDSERVICOPAI + ",DEITEMMENU='" + DEITEMMENU + "',NUORDEM=" + NUORDEM + ",FLVISIVELMENU='" + FLVISIVELMENU + "'" + " WHERE " + "CDSERVICO =" + CDSERVICO + ";");
+            String sql = (updateSQL + "FLFORAUSO ='" + FLFORAUSO
+                    + "',CDSERVICOPAI =" + CDSERVICOPAI
+                    + ",DETITULO='" + DETITULO
+                    + "',FLNECESSITAPF='" + FLNECESSITAPF
+                    + "',FLNECESSITAOAB='" + FLNECESSITAOAB
+                    + "',FLNECESSITACERT='" + FLNECESSITACERT
+                    + "',NUORDEM=" + NUORDEM
+                    + ",FLINTERMEDIARIO='" + FLINTERMEDIARIO
+                    + "',FLINICIAL='" + FLINICIAL
+                    + "',FLVISIVELMENU='" + FLVISIVELMENU
+                    + "',FLACESSORAPIDO='" + FLACESSORAPIDO
+                    + "'" + " WHERE " + "CDSERVICO =" + CDSERVICO + ";");
 
             System.out.println(sql);
             writer.write(sql);
