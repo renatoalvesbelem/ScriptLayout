0cc8aca4bf75182e2d019a21e935a9966625f1ae
jose.renato <Ra-481436>
2018-04-24 17:57:58 -0300

    Otimizado código

--- src/main/java/br/com/view/Principal.java
+++ src/main/java/br/com/view/Principal.java
@@ -7,59 +7,43 @@ import java.awt.event.ActionListener;
 import java.io.File;
 
 public class Principal {
-    private static JButton btSelecionarArquivoInsertTotal, btSelecionarArquivoUpdateTotal,btSelecionarArquivoUpdateFinal;
-    private static JTextField pathFileInsert,pathFileUpdateTotal,pathFileUpdateFinal;
+    private static JButton btSelecionarArquivoInsertTotal, btSelecionarArquivoUpdateTotal, btSelecionarArquivoUpdateFinal;
+    private static JTextField pathFileInsert, pathFileUpdateTotal, pathFileUpdateFinal;
 
-    public static void main(String args[]){
+    public static void main(String args[]) {
         JFrame jFrame = new JFrame();
-        jFrame.setSize(new Dimension(800,400));
+        jFrame.setSize(new Dimension(800, 400));
         JPanel jPanel = new JPanel();
-        jPanel.setSize(new Dimension(800,400));
+        jPanel.setSize(new Dimension(800, 400));
 
         btSelecionarArquivoInsertTotal = new JButton("Selecione");
-        btSelecionarArquivoInsertTotal.setSize(new Dimension(100,20));
-        btSelecionarArquivoInsertTotal.setLocation(1,1);
+        btSelecionarArquivoInsertTotal.setSize(new Dimension(100, 20));
+        btSelecionarArquivoInsertTotal.setLocation(1, 1);
         btSelecionarArquivoInsertTotal.addActionListener(new ActionListener() {
             public void actionPerformed(ActionEvent e) {
-                JFileChooser file = new JFileChooser();
-                file.setFileSelectionMode(JFileChooser.FILES_ONLY);
-                int i= file.showSaveDialog(null);
-                if (i==1){
-                    pathFileInsert.setText("");
-                } else {
-                    File arquivo = file.getSelectedFile();
-                    pathFileInsert.setText(arquivo.getPath());
-                }
+                atualizaTextField(pathFileInsert);
             }
-        } );
+        });
 
 
         pathFileInsert = new JTextField();
-        pathFileInsert.setSize(new Dimension(400,20));
+        pathFileInsert.setSize(new Dimension(400, 20));
         pathFileInsert.setEditable(false);
-        pathFileInsert.setLocation(110,1);
+        pathFileInsert.setLocation(110, 1);
 
         pathFileUpdateFinal = new JTextField();
-        pathFileUpdateFinal.setSize(new Dimension(400,20));
+        pathFileUpdateFinal.setSize(new Dimension(400, 20));
         pathFileUpdateFinal.setEditable(false);
-        pathFileUpdateFinal.setLocation(110,50);
+        pathFileUpdateFinal.setLocation(110, 50);
 
-        btSelecionarArquivoUpdateFinal =new JButton("Selecione");
-        btSelecionarArquivoUpdateFinal.setSize(new Dimension(100,20));
-        btSelecionarArquivoUpdateFinal.setLocation(1,50);
+        btSelecionarArquivoUpdateFinal = new JButton("Selecione");
+        btSelecionarArquivoUpdateFinal.setSize(new Dimension(100, 20));
+        btSelecionarArquivoUpdateFinal.setLocation(1, 50);
         btSelecionarArquivoUpdateFinal.addActionListener(new ActionListener() {
             public void actionPerformed(ActionEvent e) {
-                JFileChooser file = new JFileChooser();
-                file.setFileSelectionMode(JFileChooser.FILES_ONLY);
-                int i= file.showSaveDialog(null);
-                if (i==1){
-                    pathFileUpdateFinal.setText("");
-                } else {
-                    File arquivo = file.getSelectedFile();
-                    pathFileUpdateFinal.setText(arquivo.getPath());
-                }
+                atualizaTextField(pathFileUpdateFinal);
             }
-        } );
+        });
 
         jPanel.add(pathFileInsert);
         jPanel.add(pathFileUpdateFinal);
@@ -73,4 +57,15 @@ public class Principal {
 
     }
 
+    private static void atualizaTextField(JTextField jTextField) {
+        JFileChooser file = new JFileChooser();
+        file.setFileSelectionMode(JFileChooser.FILES_ONLY);
+        int i = file.showSaveDialog(null);
+        if (i == 1) {
+            jTextField.setText("");
+        } else {
+            File arquivo = file.getSelectedFile();
+            jTextField.setText(arquivo.getPath());
+        }
+    }
 }
